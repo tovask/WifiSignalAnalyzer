1453a6b80e23c76fda741bc765a13076c9e75bdb
Levente Kováts <22732484+tovask@users.noreply.github.com>
2018-11-04 09:16:49 +0100

    looping over the channels

--- CapturePackets.java
+++ CapturePackets.java
@@ -6,14 +6,13 @@ public class CapturePackets {
 
         RecordSubmitter rs = new RecordSubmitter(); // throws IOException
 
-
         //sudo tshark -i wlan0 -l -T fields -E quote=s -E separator=, -f "link[0] == 0x80" -e frame.number -e frame.time_epoch -e wlan_radio.channel -e wlan_radio.signal_dbm -e wlan.bssid -e wlan_mgt.fixed.beacon -e wlan_mgt.ssid -c 5 -a duration:3
 
         String command = "";
 
         command += "sudo tshark -i wlan0 -l -T fields -E quote=s -E separator=, ";
         //command += "-c 5 ";
-        //command += "-a duration:3 ";
+        command += "-a duration:5 ";
         command += "-f link[0]==0x80 "; // IEEE 802.11 Beacon frame
 
         command += "-e frame.number ";
@@ -38,33 +37,37 @@ public class CapturePackets {
         command += "-e wlan_mgt.fixed.beacon "; // Beacon interval in time unit (TU: unit of time equal to 1024 microseconds), typically 100 TU
         command += "-e wlan_mgt.ssid ";
 
+        int channel = 1;
+        while(true){
+            System.out.print(" "+channel);
+            //System.out.println("setting up the interface");
+            Runtime.getRuntime().exec("sudo iwconfig wlan0 channel "+channel).waitFor(); // throws IOException, InterruptedException
+            Runtime.getRuntime().exec("sudo ifconfig wlan0 down").waitFor(); // throws IOException, InterruptedException
+            Runtime.getRuntime().exec("sudo ifconfig wlan0 up").waitFor(); // throws IOException, InterruptedException
+            Runtime.getRuntime().exec("sudo iwconfig wlan0 mode Monitor").waitFor(); // throws IOException, InterruptedException
 
-        System.out.println("setting up the interface");
-        Runtime.getRuntime().exec("sudo iwconfig wlan0 channel 3").waitFor(); // throws IOException, InterruptedException
-        Runtime.getRuntime().exec("sudo ifconfig wlan0 down").waitFor(); // throws IOException, InterruptedException
-        Runtime.getRuntime().exec("sudo ifconfig wlan0 up").waitFor(); // throws IOException, InterruptedException
-        Runtime.getRuntime().exec("sudo iwconfig wlan0 mode Monitor").waitFor(); // throws IOException, InterruptedException
+            //System.out.println("start capturing...");
+            //System.out.println(command);
+            Process p = Runtime.getRuntime().exec(command); // throws IOException
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
 
-        System.out.println("start capturing...");
-        System.out.println(command);
-        Process p = Runtime.getRuntime().exec(command); // throws IOException
-        BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-
-        String s;
-        while ((s = stdInput.readLine()) != null) { // throws IOException
-            //System.out.println(s);
-            try {
-                rs.submitRecord(s);
-            } catch (IOException e) {
-                e.printStackTrace();
+            String s;
+            while ((s = stdInput.readLine()) != null) { // throws IOException
+                //System.out.println(s);
+                try {
+                    rs.submitRecord(s);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        }
-        System.out.println("---");
-        while ((s = stdError.readLine()) != null) { // throws IOException
-            System.out.println(s);
-        }
+            //System.out.println("---");
+            while ((s = stdError.readLine()) != null) { // throws IOException
+                //System.out.println(s);
+            }
 
+            channel = (channel)%13+1;
+        }
 
     }
 }
